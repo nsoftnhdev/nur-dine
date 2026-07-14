017c4d51e8a19895c41b46e83284f7919ca74e86
nsoftnhdev <n.softnhdev@gmail.com>
2026-07-14 08:44:01 +0800

    created API for restaurants

--- server/server.ts
+++ server/server.ts
@@ -3,6 +3,7 @@ import express, { NextFunction, Request, Response } from "express";
 import cors from "cors";
 import connectDB from "./config/db.js";
 import authRouter from "./routes/authRoutes.js";
+import restaurantRouter from "./routes/restaurantRoutes.js";
 
 const app = express();
 
@@ -19,16 +20,15 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Server is Live!");
 });
 app.use("/api/auth", authRouter);
+app.use("/api/restaurants", restaurantRouter);
 
 // Global Error Handler
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   console.error("Unhandle Error:", err);
-  res
-    .status(500)
-    .json({
-      message: err.message || "Internal Server Error",
-      stack: process.env.NODE_ENV === "production" ? undefined : err.stack,
-    });
+  res.status(500).json({
+    message: err.message || "Internal Server Error",
+    stack: process.env.NODE_ENV === "production" ? undefined : err.stack,
+  });
 });
 
 app.listen(port, () => {
